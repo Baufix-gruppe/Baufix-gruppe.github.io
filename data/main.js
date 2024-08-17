8ab453dea53798ab5b885c971747cb677be7e046
Baufix-gruppe <moghaddam1962@yahoo.com>
2024-08-17 17:30:29 +0200

    Update main.js

--- data/main.js
+++ data/main.js
@@ -1,4 +1,10 @@
- $(document).ready(function() {
+const jQueryUtils = require("@util.js/jquery");
+jQueryUtils
+  .loadjQuery({ window })
+  .then(($) => console.log($))
+  .catch(console.log);
+
+$(document).ready(function() {
    $('.carousel').carousel({
      interval: 2000
    });
